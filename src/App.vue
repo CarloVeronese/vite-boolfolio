4fc4eabfca313c5a500804670cdbc58e04642992
CarloVeronese <carlo.veronese97@gmail.com>
2024-01-15 17:20:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import ProjectIndex from './pages/projects/ProjectIndex.vue';
+  export default {
+    components: {
+      ProjectIndex
+    }
+
+  }
+</script>
+
+<template>
+  <ProjectIndex />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
---
+++ src/pages/projects/ProjectIndex.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios
+            .get('http://127.0.0.1:8000/api/projects')
+            .then((res) => {
+                console.log(res.data.results.data);
+                this.projects = res.data.results.data;
+            })
+        }
+    },
+    created() {
+        this.fetchProjects()
+    }
+
+}
+</script>
+
+<template>
+</template>
+
+<style>
+</style>
